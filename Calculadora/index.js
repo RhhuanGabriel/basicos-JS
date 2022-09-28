454129fb56f8dccf0306f63d886f5658733642d9
RhhuanGabriel <rhuan.vasconcelos2016@gmail.com>
2022-09-27 21:58:43 -0300

    Dia 2

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -1,19 +1,26 @@
-var n1 = document.querySelector('#n1')
-var n2 = document.querySelector('#n2')
-var resul = document.querySelector('#resultado')
+var n1 = document.getElementById("n1")
+var n2 = document.getElementById("n2")
+var reset = document.getElementById("botaoReset")
+var resul = document.getElementById("resultado")
 
 function Somar(){
-    resul.innerHTML = parseInt(n1.value) + parseInt(n2.value)
+    resul.innerHTML = parseFloat(n1.value) + parseFloat(n2.value)
 }
 
 function Subtrair(){
-    resul.innerHTML = parseInt(n1.value) - parseInt(n2.value)
+    resul.innerHTML = parseFloat(n1.value) - parseFloat(n2.value)
 }
 
 function Multiplicar(){
-    resul.innerHTML = parseInt(n1.value) * parseInt(n2.value)
+    resul.innerHTML = parseFloat(n1.value) * parseFloat(n2.value)
 }
 
 function Dividir(){
-    resul.innerHTML = parseInt(n1.value) / parseInt(n2.value)
+    resul.innerHTML = parseFloat(n1.value) / parseFloat(n2.value)
 }
+
+reset.addEventListener('click', () => {
+    n1.forEach(n1 => n1.value = '')
+    n2.forEach(n2 => n2.value = '')
+    resul.forEach(resul => resul.value = '')
+})
